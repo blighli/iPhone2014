fb7b502acf3db5732704b01bfb0d71b07a518f88
docee <docee@163.com>
2014-09-30 15:26:31 +0800

    初始化项目

---
+++ yxb144325002黄盼青/Project1-Cal/Project1-Cal/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Project1-Cal
+//
+//  Created by 黄盼青 on 14-9-30.
+//  Copyright (c) 2014年 docee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
