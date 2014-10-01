66c4f691cc1de8e06830d47dfc2251f23d4912b1
wangshu <315876632@qq.com>
2014-10-01 22:46:04 +0800

    作业初版，尚未完成。

---
+++ 21451141王术/myproject1/myproject1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  myproject1
+//
+//  Created by  ws on 14-10-1.
+//  Copyright (c) 2014年  ws. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
