796e8e79cd97b6f7ee0df3b906270252269f8975
Van <fan543119@gmail.com>
2014-10-07 16:32:41 +0800

    cal 0.1
    使用 NSCalendar 等 API 模拟实现 cal 命令

---
+++ yxb144325003潘汝范/project1/project1/printCalendar.h
@@ -0,0 +1,24 @@
+//
+//  printCalendar.h
+//  project1
+//
+//  Created by Van on 14-10-6.
+//  Copyright (c) 2014年 Van. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+static const char  WeekdayName[] = {
+    " Sun  Mon  Tue  Wed  Thu  Fri  Sat "
+};
+static const char *monthName[]= {
+    "January", "February", "March", "April",
+    "May", "June", "July", "August",
+    "September", "October", "November", "December",
+};
+@interface printCalendar : NSObject
+-(void) printCalendarWith:(NSDateComponents *)comps :(BOOL)inSingMonth;
++(void) printSpecialMonth:(NSDateComponents *)comps :(BOOL)inSingMonth;
++(void) printTitle:(NSDateComponents *)comps :(BOOL)inSingMonth;
++(long) ConvertToJulian:(NSDateComponents *)comps :(NSUInteger)dayNum;
+@end
