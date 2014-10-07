2b89ea30a67bb345a0f8c1e0b8733ba5a45007d5
mzorro <mzorrox@gmail.com>
2014-10-07 23:30:51 +0800

    完成第一个版本。

--- 21451059彭君健/Project1/Project1/main.m
+++ 21451059彭君健/Project1/Project1/main.m
@@ -7,17 +7,61 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "MyCalendar.h"
+
+// Add exctention to NSArray
+@interface NSArray (ArgumentConvenience)
++ (NSArray *) arrayWithArguments:(const char *[]) argv
+                           count:(int) argc;
+@end
+
+@implementation NSArray (ArgumentConvenience)
++ (NSArray *)arrayWithArguments:(const char *[])argv count:(int)argc {
+    NSMutableArray *array = [NSMutableArray arrayWithCapacity:argc];
+    for (int i = 1; i < argc; i++) {
+        [array addObject:[NSString stringWithCString:argv[i]
+                                            encoding:NSASCIIStringEncoding]];
+    }
+    return array;
+}
+@end
+
+void systemcal(NSArray *args) {
+    NSMutableString *cmd = [NSMutableString stringWithString: @"cal"];
+    for (NSString *arg in args) {
+        [cmd appendFormat:@" %@", arg];
+    }
+    NSLog(@"%@", cmd);
+    system([cmd cStringUsingEncoding:NSASCIIStringEncoding]);
+}
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        NSMutableString *arg = [NSMutableString stringWithString: @"cal"];
-        for (int i = 1; i < argc; i++) {
-            [arg appendFormat:@" %@",
-             [NSString stringWithCString:argv[i]
-                                encoding:NSASCIIStringEncoding]];
+        NSArray *args = [NSArray arrayWithArguments:argv count:argc];
+        MyCalendar *cal = nil;
+        switch (args.count) {
+            case 0:
+                cal = [[MyCalendar alloc] init];
+                [cal printMonth];
+                break;
+            case 1:
+                cal = [[MyCalendar alloc] initWithYear:[args[0] intValue]];
+                [cal printYear];
+                break;
+            case 2:
+                if ([@"-m" compare:args[0]] == NSOrderedSame) {
+                    cal = [[MyCalendar alloc] initWithMonth:[args[1] intValue]];
+                } else {
+                    cal = [[MyCalendar alloc] initWithYear:[args[1] intValue]
+                                                  andMonth:[args[0] intValue]];
+                }
+                [cal printMonth];
+                break;
+            default:
+                systemcal(args);
+                break;
         }
-        NSLog(@"%@", arg);
-        system([arg cStringUsingEncoding:NSASCIIStringEncoding]);
+        printf("\n");
     }
     return 0;
 }
