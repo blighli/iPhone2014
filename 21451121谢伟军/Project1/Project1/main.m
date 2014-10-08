1060f1c0181e2dd403309649e8e52741e782d801
XVXVXXX <373203339@qq.com>
2014-10-08 19:17:35 +0800

    Initial Commit

---
+++ 21451121谢伟军/Project1/Project1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Project1
+//
+//  Created by xvxvxxx on 14-10-8.
+//  Copyright (c) 2014年 谢伟军. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
