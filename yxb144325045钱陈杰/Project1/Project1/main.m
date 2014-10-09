85a64a71a0d8fd97dd1dc6b00ee8a21af876bd22
ZhaoXM <zhaoxiaomeng324@gmail.com>
2014-10-09 14:17:56 +0800

    Initial Commit

---
+++ yxb144325045钱陈杰/Project1/Project1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Project1
+//
+//  Created by ZhaoXM on 14-10-9.
+//  Copyright (c) 2014年 qianchj. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
