287574a75501b32526434c7152dcba97e9ea4559
XVXVXXX <373203339@qq.com>
2014-10-11 00:09:10 +0800

    main.m注释

--- 21451121谢伟军/Project1/Project1/main.m
+++ 21451121谢伟军/Project1/Project1/main.m
@@ -12,19 +12,19 @@
 #import "DrawPic.h"
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here..
         DrawPic *myPic = [DrawPic alloc];
         switch (argc) {
                 
-                
+            //命令行只有一个参数 ./cal  查询当月日历
             case 1:{
                 myPic = [myPic initWithToday];
                 [myPic draw];
             }
                 break;
                 
-                
+            //命令行有两个参数
             case 2:{
+                // ./cal 2014  查询2014年整年日历
                 if (atoi(argv[1])>=1 && atoi(argv[1])<=9999) {
                     for (NSInteger month  = 1; month <= 12; month++) {
                         myPic = [myPic initWithInputDayWithYear:atoi(argv[1])
@@ -37,8 +37,9 @@ int main(int argc, const char * argv[]) {
             }
                 break;
                 
-                
+            //命令行三个参数
             case 3:{
+                // ./cal -m 11 查询当年11月日历
                 if (strcmp(argv[1], "-m") == 0) {
                     if (atoi(argv[2]) >= 1 && atoi(argv[2]) <= 12) {
                         myPic = [myPic initWithToday];
@@ -47,7 +48,9 @@ int main(int argc, const char * argv[]) {
                     }else{
                         printf("cal: %s is neither a month number (1..12) nor a name\n",argv[2]);
                     }
-                }else{
+                }
+                // ./cal 10 2014 查询2014年10月日历
+                else{
                     if (atoi(argv[2])<1 || atoi(argv[2])>9999)
                         printf("cal: year %s not in range 1..9999\n",argv[2]);
                     else{
@@ -64,6 +67,7 @@ int main(int argc, const char * argv[]) {
                 
                 
             default:
+                //printf("cal: 太多参数啦 （￣д￣）\n");
                 break;
                 
                 
