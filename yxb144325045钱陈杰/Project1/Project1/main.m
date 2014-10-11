b8aa7dd552a74a93ef49ad2a7c7e20a37eb47f56
ZhaoXM <zhaoxiaomeng324@gmail.com>
2014-10-11 17:02:34 +0800

    钱陈杰 日历程序第一次提交

--- yxb144325045钱陈杰/Project1/Project1/main.m
+++ yxb144325045钱陈杰/Project1/Project1/main.m
@@ -1,21 +1,49 @@
-//
-//  main.m
-//  Project1
-//
-//  Created by ZhaoXM on 14-10-9.
-//  Copyright (c) 2014年 qianchj. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
+#import "Calendar.h"
 
 int main(int argc, const char * argv[])
 {
 
-    @autoreleasepool {
-        
-        // insert code here...
-        NSLog(@"Hello, World!");
-        
+    Calendar *cal=[[Calendar alloc]init];
+    NSDate *date=[NSDate date];
+    NSCalendar *calendar=[NSCalendar currentCalendar];
+    NSDateComponents *comps=[calendar components:(NSYearCalendarUnit | NSMonthCalendarUnit |NSDayCalendarUnit) fromDate:date];
+    int year=[comps year];
+    int month=[comps month];
+    char c[]="-m";
+    switch (argc) {
+        case 1:
+            [cal showMonth: year:month];
+            break;
+        case 2:
+            if(atoi(argv[1])<=0||atoi(argv[1])>9999){
+                printf("year %s not in 1..9999\n",argv[1]);
+                return 1;
+            }
+            
+            [cal showYear:atoi(argv[1])];
+            break;
+        case 3:
+            if(!strcmp(c, argv[1])){
+                if(atoi(argv[2])>12||atoi(argv[2])<1){
+                    printf("%s is not (1..12) or a name\n",argv[2]);
+                    return 1;
+                }
+                [cal showMonth:year:atoi(argv[2])];
+            }else{
+                if(atoi(argv[1])>12||atoi(argv[1])<1){
+                    printf("%s is not a month number (1..12) or a name\n",argv[1]);
+                    return 1;
+                }
+                if(atoi(argv[2])<=0||atoi(argv[2])>9999){
+                    printf("year %s not in 1..9999\n",argv[2]);
+                    return 1;
+                }
+                [cal showMonth:atoi(argv[2]):atoi(argv[1])];
+            }
+            break;
+        default:
+            break;
     }
     return 0;
 }
