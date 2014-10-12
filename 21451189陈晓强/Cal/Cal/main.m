13fe546867e95ca856d4acebbcfc41e5c86f0eb0
zxccft0123 <236003942@qq.com>
2014-10-12 17:10:54 +0800

    Initial Commit

---
+++ 21451189陈晓强/Cal/Cal/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Cal
+//
+//  Created by 陈晓强 on 14-10-12.
+//  Copyright (c) 2014年 陈晓强. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
