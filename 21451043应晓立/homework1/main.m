7b01ae2f4062ff8401d67e562ebe8de84c292f15
yingxiaoli <yingxl1992@163.com>
2014-10-12 14:55:10 +0800

    基本完成，未考虑cal 9 1752

---
+++ 21451043应晓立/homework1/main.m
@@ -0,0 +1,66 @@
+//
+//  main.m
+//  homework1
+//
+//  Created by yingxl1992 on 14-10-11.
+//  Copyright (c) 2014年 yingxl1992. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "MyCal.h"
+
+#define NSLog(FORMAT, ...) printf("%s\n", [[NSString stringWithFormat:FORMAT, ##__VA_ARGS__] UTF8String])
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        MyCal *util=[MyCal new];
+        
+        if(argc==1){
+            [util outputMonthByYM:-1 month:-1];
+        }else if (argc==2){
+            NSString *year=[NSString stringWithUTF8String:argv[1]];
+            NSInteger y=[year integerValue];
+            if(y>=1&&y<=9999){
+                NSInteger len=[year length];
+                NSString *yearline=[util addSpace:64 string:year len:len];
+                NSLog(@"%@",yearline);
+                NSLog(@"");
+                [util outputYear:y];
+            }else{
+                NSLog(@"cal: year is not in range 1..9999");
+            }
+        }else if (argc==3){
+            NSString *str2=[NSString stringWithUTF8String:argv[1]];
+            if([[str2 substringToIndex:1] compare:@"-"]==NSOrderedSame){
+                if([str2 compare:@"-m"]==NSOrderedSame){
+                    NSString *str3=[NSString stringWithUTF8String:argv[2]];
+                    NSInteger month=[str3 integerValue];
+                    if(month>=1&&month<=12){
+                        [util outputMonthByYM:-1 month:month];
+                    }else{
+                        NSLog(@"cal: month is not in range 1..12");
+                    }
+                }else{
+                    NSLog(@"cal: illegal option");
+                }
+            }else{
+                NSInteger month=[str2 integerValue];
+                
+                NSString *str3=[NSString stringWithUTF8String:argv[2]];
+                NSInteger year=[str3 integerValue];
+                if(month>=1&&month<=12){
+                    if(year<=9999&&year>=1){
+                        [util outputMonthByYM:year month:month];
+                    }
+                    else{
+                        NSLog(@"cal: year is not in range 1..9999");
+                    }
+                }else{
+                    NSLog(@"cal: month is not in range 1..12");
+                }
+            }
+        }
+    }
+    
+    return 0;
+}
