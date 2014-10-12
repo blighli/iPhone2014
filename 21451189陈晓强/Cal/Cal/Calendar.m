62b11bb9528ce37ff12770da9e142aa373bc0da6
zxccft0123 <236003942@qq.com>
2014-10-12 17:38:30 +0800

    陈晓强的作业
    修改版

--- 21451189陈晓强/Cal/Cal/Calendar.m
+++ 21451189陈晓强/Cal/Cal/Calendar.m
@@ -206,10 +206,7 @@
 {
     arrayChineseCharacter = [NSArray arrayWithObjects:@"一",@"二",@"三",@"四",@"五",@"六",@"七",@"八",
                              @"九",@"十",@"十一",@"十二",nil];
-    //    NSString *monthOfCharacterString = [arrayChineseCharacter objectAtIndex:_month-1];
-    //    NSLog(@"%lu %@",_month, [arrayChineseCharacter objectAtIndex:_month]);
     const char *monthOfCharacter = [[arrayChineseCharacter objectAtIndex:_month-1] UTF8String];
-    printf("\n");
     printf("     %s月 %lu \n", monthOfCharacter , _year);
     printf("日 一 二 三 四 五 六\n");
     
