27b54bc3aad0eca1c5207fd73f93b881e776340a
xuyouyang <247082642@qq.com>
2014-10-12 13:10:32 +0800

    项目建立

---
+++ 21451074徐有阳/project1/project1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  project1
+//
+//  Created by xuyouyang on 14-10-12.
+//  Copyright (c) 2014年 zju-cst. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
