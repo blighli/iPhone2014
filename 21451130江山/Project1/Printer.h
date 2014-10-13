6867247335d57f1be11b761e8f646717725a5738
jiang-s <619575254@qq.com>
2014-10-13 18:01:08 +0800

    v1.0

---
+++ 21451130江山/Project1/Printer.h
@@ -0,0 +1,19 @@
+//
+//  Printer.h
+//  Project1
+//
+//  Created by 江山 on 10/11/14.
+//  Copyright (c) 2014 江山. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Printer: NSObject{
+    
+}
+-(void) yearPrint: (int)year;
+
+-(void) monthPrint: (int)year :(int)month;
+
+@end
+
