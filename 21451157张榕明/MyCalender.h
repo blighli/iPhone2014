4ff6c6dad7adf5a6e7121b9564da3c36a0d5bb1a
RYXIAOMING <568559266@qq.com>
2014-10-16 21:30:08 +0800

    第一次作业
    张榕明第一次的作业

---
+++ 21451157张榕明/MyCalender.h
@@ -0,0 +1,14 @@
+//
+//  MyCalender.h
+//  lzxlzx
+//
+//  Created by hu on 14-10-16.
+//  Copyright (c) 2014年 hu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyCalender : NSObject
+-(void) printfCalender: (int)year month:(int)month;
+@end
+
