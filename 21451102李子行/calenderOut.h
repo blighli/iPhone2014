f01fe6f0f4fc174a4b36eff01c828ece598c3262
sewssb001 <sewssb001@gmail.com>
2014-10-16 14:54:15 +0800

    李子行的作业
    作业

---
+++ 21451102李子行/calenderOut.h
@@ -0,0 +1,16 @@
+//
+//  calenderOut.h
+//  calender
+//
+//  Created by laotou on 14-10-14.
+//  Copyright (c) 2014年 laotou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface calenderOut : NSObject
++(void) year:(int)year month:(int)month;
+@end
+
+
+
