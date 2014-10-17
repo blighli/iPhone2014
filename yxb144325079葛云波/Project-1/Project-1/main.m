bd3659f3fa47d14164c203033b1718d66423b5f4
geyunbo <237562349@qq.com>
2014-10-18 00:07:58 +0800

    Initial Commit

---
+++ yxb144325079葛云波/Project-1/Project-1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Project-1
+//
+//  Created by 葛 云波 on 14-10-18.
+//  Copyright (c) 2014年 葛 云波. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
