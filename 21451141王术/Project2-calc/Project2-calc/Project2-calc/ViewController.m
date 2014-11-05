7e301e12f27441aca97a7880aa7c1b80481a2079
wangshu <315876632@qq.com>
2014-11-05 18:37:07 +0800

    Initial Commit

---
+++ 21451141王术/Project2-calc/Project2-calc/Project2-calc/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Project2-calc
+//
+//  Created by  ws on 14/11/5.
+//  Copyright (c) 2014年 ws. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
