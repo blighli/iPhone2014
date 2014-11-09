5daebc01a5ea7d9e5ef0d688795b1a1330a8415a
sewsssb001 <sewssb001@gmail.com>
2014-11-09 16:09:27 +0800

    作业
    作业

---
+++ 21451102李子行/My-Caculator/My-Caculator/AppDelegate.h
@@ -0,0 +1,17 @@
+﻿//
+//  AppDelegate.h
+//  My-Caculator
+//
+//  Created by lzx on 14/11/9.
+//  Copyright (c) 2014年 lzx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
