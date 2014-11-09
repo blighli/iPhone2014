47e36f73eec09f6a08c312a9669781761cb64a51
janestar <janestar92@163.com>
2014-11-09 21:29:12 +0800

    homework

---
+++ 21451176刘良建_2_3次作业/Calculator/Calculator/CalculatorAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CalculatorAppDelegate.h
+//  Calculator
+//
+//  Created by JANESTAR on 14-11-5.
+//  Copyright (c) 2014年 JANESTAR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
