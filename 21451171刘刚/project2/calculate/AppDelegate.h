c1d12a2c082dfcd457652cdb8b8f7a95edd752fb
liug222 <853381092@qq.com>
2014-11-09 22:52:02 +0800

    project2and3

---
+++ 21451171刘刚/project2/calculate/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  calculate
+//
+//  Created by liug on 14-11-5.
+//  Copyright (c) 2014年 liug. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
