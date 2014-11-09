e1cfd878dd817b9f236658e303e10147e2169070
XVXVXXX <373203339@qq.com>
2014-11-10 00:04:31 +0800

    Project3 增删改

--- 21451121谢伟军/Project3/Project3/ViewController.m
+++ 21451121谢伟军/Project3/Project3/ViewController.m
@@ -105,5 +105,63 @@
     }
 }
 
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    NSString *task = self.tasks[indexPath.row];
+    NSString *title = @"修改任务？";
+    
+    UIAlertView *alert = [[UIAlertView alloc]initWithTitle:title message:[NSString stringWithFormat:@"当前任务:%@",task] delegate:self cancelButtonTitle:@"取消" otherButtonTitles:@"确定", nil];
+    // alertViewStyle 样式----密码
+    // alert.alertViewStyle = UIAlertViewStyleSecureTextInput;
+    // alertViewStyle 样式----一般的文本输入框
+    alert.alertViewStyle = UIAlertViewStylePlainTextInput;
+    // alertViewStyle 样式----用户名及密码登录框
+    // alert.alertViewStyle = UIAlertViewStyleLoginAndPasswordInput;
+    // alertViewStyle 样式----标签显示
+    // alert.alertViewStyle = UIAlertViewStyleDefault;
+    
+    // 用户名密码的情况下有两个文本框
+    UITextField *textField = [alert textFieldAtIndex:0];
+    textField.text = task;
+    // 关键代码,通过tag将点击的行号带给alertView的代理方法,还可以通过利用代理即控制器的成员进行 行号 的传递~
+    textField.tag = indexPath.row;
+    // 显示alertView
+    [alert show];
+    
+    
+    
+}
+
+
+#pragma mark - UIAlertViewDelegate的代理方法
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    // 查看点击了alertView里面的哪一个按钮,取消按钮是 0
+    NSLog(@"alertView里面的按钮index---%d",buttonIndex);
+    if (buttonIndex == 0) {
+        // 0代表取消按钮
+        return;
+    }else if (buttonIndex == 1){
+        // 1代表确定按钮,更新数据源,重新加载数据
+        UITextField *textField = [alertView textFieldAtIndex:0];
+        NSString *newTask = [textField text];
+        // robust判断
+        if ([newTask isEqualToString:@""]) {
+            return;
+        }
+        // 先更新数据源
+        int row = textField.tag;
+
+        self.tasks[row] = newTask;
+        // 再,全部重新加载数据
+        // [_tableView reloadData];
+        
+        // 最好是,局部刷新数据,通过row生成一个一个indexPath组成数组
+        NSIndexPath *indexPath = [NSIndexPath indexPathForRow:row inSection:0];
+        NSArray *indexPaths = @[indexPath];
+        [self.taskTable reloadRowsAtIndexPaths:indexPaths withRowAnimation:UITableViewRowAnimationLeft];
+        [self.tasks writeToFile:[self docPath] atomically:YES];
+    }
+}
 
 @end
