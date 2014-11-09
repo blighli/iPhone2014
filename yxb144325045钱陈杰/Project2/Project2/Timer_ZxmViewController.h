ad1700044c416f66159fe3b9c8d9a2d5ae573d13
ZhaoXM <zhaoxiaomeng324@gmail.com>
2014-11-09 11:25:32 +0800

    Initial Commit

---
+++ yxb144325045钱陈杰/Project2/Project2/Timer_ZxmViewController.h
@@ -0,0 +1,13 @@
+//
+//  Timer_ZxmViewController.h
+//  Project2
+//
+//  Created by ZhaoXM on 14-11-9.
+//  Copyright (c) 2014年 qianchj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Timer_ZxmViewController : UIViewController
+
+@end
---
+++ yxb144325045钱陈杰/Project2/Project2/Timer_ZxmViewController.m
@@ -0,0 +1,29 @@
+//
+//  Timer_ZxmViewController.m
+//  Project2
+//
+//  Created by ZhaoXM on 14-11-9.
+//  Copyright (c) 2014年 qianchj. All rights reserved.
+//
+
+#import "Timer_ZxmViewController.h"
+
+@interface Timer_ZxmViewController ()
+
+@end
+
+@implementation Timer_ZxmViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
