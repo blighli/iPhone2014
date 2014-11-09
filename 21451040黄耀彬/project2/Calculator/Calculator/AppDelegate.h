a7c9c6cbd1f526f4eae12c8375ec255ab9b898b9
krustoh <339031206@qq.com>
2014-11-08 21:40:07 -0800

    Project2
    finish the project2—calculator

---
+++ 21451040黄耀彬/project2/Calculator/Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by 黄耀彬 on 14-11-7.
+//  Copyright (c) 2014年 黄耀彬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 21451040黄耀彬/project2/Calculator/Calculator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Calculator
+//
+//  Created by 黄耀彬 on 14-11-7.
+//  Copyright (c) 2014年 黄耀彬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
