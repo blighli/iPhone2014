c4c2ca19fae86baa4d9fe86b8ed47b304898b3b6
飞翔的猫 <478931799@qq.com>
2014-11-09 20:56:48 +0800

    first commit

---
+++ yxb144325005陈凯峰/homeword2/homeword2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  homeword2
+//
+//  Created by jingcheng407 on 14-11-9.
+//  Copyright (c) 2014年 chenkaifeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ yxb144325005陈凯峰/homeword2/homeword2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  homeword2
+//
+//  Created by jingcheng407 on 14-11-9.
+//  Copyright (c) 2014年 chenkaifeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
