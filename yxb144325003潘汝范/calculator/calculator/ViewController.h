47d8767a98ab7bea61101f677c3e35d5620cf3fd
Van <fan543119@gmail.com>
2014-11-09 16:48:16 +0800

    实现UI autolayout 适应各个尺寸的iOS Device，并添加了icon

---
+++ yxb144325003潘汝范/calculator/calculator/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  calculator
+//
+//  Created by Van on 14/11/9.
+//  Copyright (c) 2014年 Van. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *showLabel;
+
+
+@end
+
