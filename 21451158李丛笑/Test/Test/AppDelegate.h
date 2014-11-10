f010500585d4ee7aa307624a610d6cc2dd0d96e4
licongxiao <lcx.chrislee@163.com>
2014-11-10 13:29:20 +0800

    test

---
+++ 21451158李丛笑/Test/Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by 李丛笑 on 14/11/5.
+//  Copyright (c) 2014年 lcx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 21451158李丛笑/Test/Test/ViewController.h
@@ -0,0 +1,51 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by 李丛笑 on 14/11/5.
+//  Copyright (c) 2014年 lcx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+{
+    NSMutableString * textstr;
+    NSMutableString * numstr;
+}
+
+@property (weak, nonatomic) IBOutlet UITextView *textView;
+
+//-(void) settstr: (NSMutableString *)textstr;
+
+
+- (IBAction)zero;
+- (IBAction)dot;
+- (IBAction)one;
+- (IBAction)two;
+- (IBAction)three;
+- (IBAction)four;
+- (IBAction)five;
+- (IBAction)six;
+- (IBAction)seven;
+- (IBAction)eight;
+- (IBAction)nine;
+- (IBAction)plus;
+- (IBAction)minus;
+- (IBAction)multiple;
+- (IBAction)divide;
+- (IBAction)left;
+- (IBAction)right;
+- (IBAction)percent;
+- (IBAction)back;
+- (IBAction)ac;
+- (IBAction)mplus;
+- (IBAction)mminus;
+- (IBAction)mc;
+- (IBAction)mr;
+- (IBAction)equles;
+
+
+
+@end
+
