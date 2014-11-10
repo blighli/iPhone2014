ff902f36a0d096a2b31f000c3429fddb0f9856d7
JunsionGu <1094074122@qq.com>
2014-11-10 17:24:32 +0800

    Initial Commit

---
+++ 21451202顾准新/Calculator/Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by 顾准新 on 14-11-10.
+//  Copyright (c) 2014年 顾准新. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 21451202顾准新/Calculator/Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by 顾准新 on 14-11-10.
+//  Copyright (c) 2014年 顾准新. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
