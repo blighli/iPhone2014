febd4d49ff2999ee6ac5021d35b640177b2c423d
wangluyao <526255059@qq.com>
2014-11-10 10:55:41 +0800

    sdsd
    sdsds

--- 21451173梁斐然/Project1/myCalendar.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@interface myCalendar : NSObject
-
--(void)showCurrentMonth;
--(void)showCurrentYearWithMonth:(NSInteger)month;
--(void)showAllTheYear:(NSInteger)year;
--(void)showMonth:(NSInteger)month andYear:(NSInteger)year;
-@end
