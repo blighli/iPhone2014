25f845f06af3d99b743a46dfc272cd53a895c474
rth211 <rth211@qq.com>
2014-11-11 00:19:14 +0800

    第二次作业和第三次作业

---
+++ yxb144325208任天恒/listdemo/listdemo/ItemViewController.h
@@ -0,0 +1,34 @@
+//
+//  ItemViewController.h
+//  listdemo
+//
+//  Created by rth on 14/11/9.
+//  Copyright (c) 2014年 MSE.ZJU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ItemViewController;
+@class ListItem;
+
+@protocol ItemViewControllerDelegate <NSObject>
+-(void)ItemViewControllerDidCancel:(ItemViewController*)controller;
+
+-(void)ItemViewController:(ItemViewController *)controller didFinishAddingItem:(ListItem *)item;  //增加元素
+
+-(void)ItemViewcontroller:(ItemViewController*)controller
+           didFinishEditingItem:(ListItem*)item;  //修改元素
+
+@end
+
+@interface ItemViewController : UITableViewController<UITextFieldDelegate>
+
+@property(nonatomic,weak) id <ItemViewControllerDelegate> delegate;
+- (IBAction)Done:(id)sender;
+
+- (IBAction)Cancel:(id)sender;
+@property (weak, nonatomic) IBOutlet UITextField *textfield;
+
+@property(nonatomic,strong) ListItem *itemToEdit;
+
+@end
