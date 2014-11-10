f0b8fd80de46532b89f4b23fbbe4178dc18997a0
rth211 <rth211@qq.com>
2014-11-11 00:44:59 +0800

    第二次和第三次作业修改版本

--- yxb144325208任天恒/MyCalculator/MyCalculatorTests/MyCalculatorTests.m
+++ yxb144325208任天恒/MyCalculator/MyCalculatorTests/MyCalculatorTests.m
@@ -2,8 +2,8 @@
 //  MyCalculatorTests.m
 //  MyCalculatorTests
 //
-//  Created by alwaysking on 14/11/3.
-//  Copyright (c) 2014年 alwaysking. All rights reserved.
+//  Created by rth on 14/11/4.
+//  Copyright (c) 2014年 MSE.ZJU. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
