8874a96b166fef583cff7cca70fd715eaa854b10
chenps3 <guanhongchen@163.com>
2014-11-12 01:44:04 +0800

    作业3

---
+++ 21451172陈冠鸿/homework3_ToDoList/ToDoList/TDLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TDLAppDelegate.h
+//  ToDoList
+//
+//  Created by Chen.D.guanhong on 14/11/9.
+//  Copyright (c) 2014年 Chen.D.guanhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TDLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 21451172陈冠鸿/homework3_ToDoList/ToDoList/TDLToDoListViewController.h
@@ -0,0 +1,13 @@
+//
+//  TDLToDoListViewController.h
+//  ToDoList
+//
+//  Created by Chen.D.guanhong on 14/11/10.
+//  Copyright (c) 2014年 Chen.D.guanhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TDLToDoListViewController : UITableViewController
+-(IBAction)unwindToList:(UIStoryboardSegue *)segue;
+@end
