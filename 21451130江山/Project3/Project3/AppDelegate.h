5c95518af0c7b3dfd10db6a6454953aa7c77907c
jiang-s <619575254@qq.com>
2014-11-11 22:19:12 +0800

    first commit

---
+++ 21451130江山/Project3/Project3/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Project3
+//
+//  Created by 江山 on 11/11/14.
+//  Copyright (c) 2014 jiangshan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ 21451130江山/Project3/Project3/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Project3
+//
+//  Created by 江山 on 11/11/14.
+//  Copyright (c) 2014 jiangshan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
