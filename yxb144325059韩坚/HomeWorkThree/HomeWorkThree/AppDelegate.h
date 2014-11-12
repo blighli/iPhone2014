334c6db72ad04546189fe5cbee022ac6aafcf691
hjandyz <281735815@qq.com>
2014-11-12 12:36:14 +0800

    2
    2

---
+++ yxb144325059韩坚/HomeWorkThree/HomeWorkThree/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  HomeWorkThree
+//
+//  Created by HJ on 14/11/5.
+//  Copyright (c) 2014年 HJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+NSString *docPath(void);
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate>
+{
+    UITableView *taskTable;
+    UITextField *taskField;
+    UIButton *insertButton;
+    NSMutableArray *tasks;
+}
+
+- (void)addTask:(id)sender;
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
