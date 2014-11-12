5dc1f06de26724d87979b706842e8bb6a07c1835
hjandyz <281735815@qq.com>
2014-11-12 12:35:27 +0800

    1
    1

---
+++ yxb144325059韩坚/HomeWorkTwo/HomeWorkTwo/FormulaStringCalcUtility.h
@@ -0,0 +1,19 @@
+//
+//  FormulaStringCalcUtility.h
+//  HomeWorkTwo
+//
+//  Created by HJ on 14/11/4.
+//  Copyright (c) 2014年 HJ. All rights reserved.
+//
+
+#ifndef HomeWorkTwo_FormulaStringCalcUtility_h
+#define HomeWorkTwo_FormulaStringCalcUtility_h
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface FormulaStringCalcUtility : NSObject
++ (NSString *)calcComplexFormulaString:(NSString *)formula;
+@end
+
+#endif
---
+++ yxb144325059韩坚/HomeWorkTwo/HomeWorkTwo/ViewController.h
@@ -0,0 +1,54 @@
+//
+//  ViewController.h
+//  HomeWorkTwo
+//
+//  Created by HJ on 14/11/3.
+//  Copyright (c) 2014年 HJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "FormulaStringCalcUtility.h"
+#import "RegexKitLite.h"
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *display;
+
+- (void) senMclickwithBlock: (void(^)()) myblock and:(UIButton *) sender;
+- (void) senbtclickwithBlock: (void(^)()) myblock and:(UIButton *) sender;
+- (BOOL) stringToCaculator;
+
+//数字键
+- (IBAction)clickDigit:(UIButton *)sender;
+//点
+- (IBAction)clickPoint:(UIButton *)sender;
+//退格
+- (IBAction)clickDelete:(UIButton *)sender;
+//等于
+- (IBAction)clickEqual:(UIButton *)sender;
+//加号
+- (IBAction)clickAdd:(UIButton *)sender;
+//减号
+- (IBAction)clickMinus:(UIButton *)sender;
+//乘以
+- (IBAction)clickMultiply:(UIButton *)sender;
+//除以
+- (IBAction)clickDivide:(UIButton *)sender;
+//AC
+- (IBAction)clickAC:(UIButton *)sender;
+//'('
+- (IBAction)clickLeftParet:(UIButton *)sender;
+//')'
+- (IBAction)clickRightParet:(UIButton *)sender;
+//'%'
+- (IBAction)clickRemain:(UIButton *)sender;
+//mc
+- (IBAction)clickMc:(UIButton *)sender;
+//m+
+- (IBAction)clickMadd:(UIButton *)sender;
+//m-
+- (IBAction)clickMminus:(UIButton *)sender;
+//mr
+- (IBAction)clickMresult:(UIButton *)sender;
+@end
+
