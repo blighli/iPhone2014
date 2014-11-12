48dd2b384e1655cc666e0c17f50e03c8ef9647a6
liug222 <853381092@qq.com>
2014-11-12 19:05:47 +0800

    project2and3

---
+++ 21451171刘刚/project3/tasklist/tasklist/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tasklist
+//
+//  Created by liug on 14-11-8.
+//  Copyright (c) 2014年 liug. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
