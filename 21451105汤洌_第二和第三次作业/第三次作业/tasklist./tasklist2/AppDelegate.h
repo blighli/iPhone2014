802dd762d5abee8043b03369af2b4a3cdf464b9a
tanglie1993 <tanglie23@163.com>
2014-11-12 11:21:25 +0800

    Revert "删除命名错误的文件"
    This reverts commit 9964289f7d8f5780be8fd4e7bdf4f4c10d3e9397.

---
+++ 21451105汤洌_第二和第三次作业/第三次作业/tasklist./tasklist2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tasklist2
+//
+//  Created by tanglie1993 on 14/11/9.
+//  Copyright (c) 2014年 com.jikexueyuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
