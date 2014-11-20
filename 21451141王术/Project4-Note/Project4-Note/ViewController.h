c2180efde279d0dbb6989b3d9658f7c71cad40c6
wangshu <315876632@qq.com>
2014-11-20 16:20:53 +0800

    Initial Commit

---
+++ 21451141王术/Project4-Note/Project4-Note/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Project4-Note
+//
+//  Created by  ws on 11/20/14.
+//  Copyright (c) 2014 ws. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 21451141王术/Project4-Note/Project4-Note/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Project4-Note
+//
+//  Created by  ws on 11/20/14.
+//  Copyright (c) 2014 ws. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
