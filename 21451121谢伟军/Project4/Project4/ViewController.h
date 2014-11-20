88f4da2f5ca60610b52f3bdc30e7892251b42652
XVXVXXX <373203339@qq.com>
2014-11-20 21:03:56 +0800

    Project4 init

---
+++ 21451121谢伟军/Project4/Project4/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Project4
+//
+//  Created by xvxvxxx on 14/11/20.
+//  Copyright (c) 2014年 谢伟军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 21451121谢伟军/Project4/Project4/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Project4
+//
+//  Created by xvxvxxx on 14/11/20.
+//  Copyright (c) 2014年 谢伟军. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
