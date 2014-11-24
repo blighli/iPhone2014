8bf1df3936fb0857f98853eff2070ec23e36b145
XVXVXXX <373203339@qq.com>
2014-11-24 21:33:55 +0800

    picture init v0.1

--- 21451121谢伟军/Project4/Project4/PictureView.h
+++ 21451121谢伟军/Project4/Project4/PictureView.h
@@ -6,8 +6,8 @@
 //  Copyright (c) 2014年 谢伟军. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
-@interface PictureView : NSObject
+@interface PictureView : UIView
 
 @end
--- 21451121谢伟军/Project4/Project4/PictureView.m
+++ 21451121谢伟军/Project4/Project4/PictureView.m
@@ -10,4 +10,12 @@
 
 @implementation PictureView
 
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
 @end
