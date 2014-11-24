133111aadae9cdab97a75c3c232aa632df6ce859
xuyouyang <247082642@qq.com>
2014-11-24 18:13:27 +0800

    绘图之后的图片可以存到本地和相册中

--- 21451074徐有阳/project4/project4/DrawViewController.m
+++ 21451074徐有阳/project4/project4/DrawViewController.m
@@ -8,6 +8,8 @@
 
 #import "DrawViewController.h"
 #import "DrawView.h"
+#import "ImageUtils.h"
+#import "Note.h"
 
 @interface DrawViewController ()
 @property (strong,nonatomic)  DrawView *drawView;
@@ -17,13 +19,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-//    CGRect viewFrame = self.view.frame;
-    CGRect viewFrame = CGRectMake(0, 64, 375, 520);
+    CGRect viewFrame = CGRectMake(0, 105, 375, 520);
     self.drawView = [[DrawView alloc]initWithFrame:viewFrame];
     [self.drawView setBackgroundColor:[UIColor redColor]];
     [self.view addSubview: self.drawView];
     [self.view sendSubviewToBack:self.drawView];
-
 }
 
 - (void)didReceiveMemoryWarning {
@@ -34,6 +34,53 @@
 - (IBAction)cancel:(id)sender {
     [self dismissViewControllerAnimated:YES completion:nil];
 }
+
+- (IBAction)save:(id)sender {
+    for (int i=1; i<16;i++) {
+        UIView *view=[self.view viewWithTag:i];
+        if ((i>=1&&i<=5)||(i>=10&&i<=15)) {
+            if (view.hidden==YES) {
+                continue;
+            }
+        }
+        view.hidden=YES;
+    }
+    UIGraphicsBeginImageContext(self.view.bounds.size);
+    [self.view.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *image=UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    UIImageWriteToSavedPhotosAlbum(image, self, @selector(image:didFinishSavingWithError:contextInfo:), nil);
+    for (int i=1; i<16; i++) {
+        if ((i>=1&&i<=5)||(i>=11&&i<=14)) {
+            continue;
+        }
+        UIView *view=[self.view viewWithTag:i];
+        view.hidden=NO;
+    }
+}
+
+- (IBAction)clear:(id)sender {
+    [self.drawView clear];
+}
+
+- (void)image:(UIImage *)image didFinishSavingWithError:(NSError *)error
+  contextInfo:(void *)contextInfo{
+    if (error != NULL){
+        //失败
+        //截屏成功
+        UIAlertView *alertView=[[UIAlertView alloc]initWithTitle:@"Error" message:@"Save Error" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:@"Cancel", nil];
+        [alertView show];
+    }
+    else{
+        //成功
+        NSString *imageName = [NSString stringWithFormat:@"%d", (int)[[NSDate date] timeIntervalSince1970]];
+        NSString *imagePath = [ImageUtils saveImage:image WithName:imageName];
+        [self dismissViewControllerAnimated:YES completion:^{
+            // 跳转到添加页面
+        }];
+    }
+}
+
 /*
 #pragma mark - Navigation
 
