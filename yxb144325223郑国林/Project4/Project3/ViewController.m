e1289e33dfe0900b5f89fdba45520fa4a340b49b
fainall1618 <fainall1618@163.com>
2014-11-24 19:58:59 +0800

    重新上传

---
+++ yxb144325223郑国林/Project4/Project3/ViewController.m
@@ -0,0 +1,26 @@
+//
+//  ViewController.m
+//  Project4
+//
+//  Created by CST-112 on 14-11-19.
+//  Copyright (c) 2014年 CST-112. All rights reserved.
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
