c5ebb39701d46161fb67440902a3cec55eea5ddd
周翔 <308086427@qq.com>
2014-11-27 02:07:41 +0800

    第四次作业

---
+++ yxb144325023周翔/WhateverNote/WhateverNote/DrawView.m
@@ -0,0 +1,21 @@
+//
+//  DrawView.m
+//  WhateverNote
+//
+//  Created by 周翔 on 14/11/26.
+//  Copyright (c) 2014年 周翔. All rights reserved.
+//
+
+#import "DrawView.h"
+
+@implementation DrawView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
