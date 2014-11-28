63109029772cb4caa043d1467e90828e5c28699d
zxccft0123 <236003942@qq.com>
2014-11-28 08:55:19 +0800

    Initial Commit

---
+++ 21451189陈晓强/EverNote/EverNote/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EverNote
+//
+//  Created by 陈晓强 on 14/11/28.
+//  Copyright (c) 2014年 陈晓强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 21451189陈晓强/EverNote/EverNote/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  EverNote
+//
+//  Created by 陈晓强 on 14/11/28.
+//  Copyright (c) 2014年 陈晓强. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
