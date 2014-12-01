6f4e3387222eb2df30bce640e7f6741fa454269b
varvelworld <varvelworld@gmail.com>
2014-12-01 13:03:31 +0800

    init iHabit

---
+++ yxb144325061陆钟豪/iHabit/iHabit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iHabit
+//
+//  Created by 陆钟豪 on 14/12/1.
+//  Copyright (c) 2014年 lzh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ yxb144325061陆钟豪/iHabit/iHabit/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iHabit
+//
+//  Created by 陆钟豪 on 14/12/1.
+//  Copyright (c) 2014年 lzh. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
