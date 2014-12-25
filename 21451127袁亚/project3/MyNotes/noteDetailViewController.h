d6648f39bf3e6ac3f4790812c5815243018c69d6
zjufrank <729754079@qq.com>
2014-12-25 23:19:17 +0800

    ToDoList
    the third homework

---
+++ 21451127袁亚/project3/MyNotes/noteDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  noteDetailViewController.h
+//  MyNotes
+//
+//  Created by Frank Yuan on 14-12-25.
+//  Copyright (c) 2014年 Frank Yuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface noteDetailViewController : UIViewController
+@property NSInteger index;
+@end
