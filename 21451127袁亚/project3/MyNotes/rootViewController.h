9b5230b0f180bb4b6eedc717f69073143d52d318
zjufrank <729754079@qq.com>
2014-12-25 23:37:22 +0800

    Revert "Revert "ToDoList""
    This reverts commit 98cf3e4c734912ed3f1eab692a78f9493c8b2d2a.

---
+++ 21451127袁亚/project3/MyNotes/rootViewController.h
@@ -0,0 +1,14 @@
+//
+//  rootViewController.h
+//  MyNotes
+//
+//  Created by Frank Yuan on 14-12-25.
+//  Copyright (c) 2014年 Frank Yuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface rootViewController : UITableViewController
+@property (nonatomic)  NSMutableArray *noteArray;
+@property (nonatomic)  NSMutableArray *dateArray;
+@end
