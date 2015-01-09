86db07388697c515b47554544be82860afe06a96
lucaho0213 <luchao_boy@163.com>
2015-01-09 16:41:04 +0800

    fianl
    大作业

---
+++ 21451198卢超/project5/MyPlane/MyPlane/CCFoePlane.m
@@ -0,0 +1,13 @@
+//
+//  CCFoePlane.m
+//  MyPlane
+//
+//  Created by jiaoshoujie on 14-12-12.
+//  Copyright (c) 2014年 jiaoshoujie. All rights reserved.
+//
+
+#import "CCFoePlane.h"
+
+@implementation CCFoePlane
+
+@end
