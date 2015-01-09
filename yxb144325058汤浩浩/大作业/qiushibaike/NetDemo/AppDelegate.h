ba9750b5abbb8d3877911ba265c92b6a8e031029
thhtom <thhemail@163.com>
2015-01-10 01:01:39 +0800

    提交作业
    第四次作业和大作业

---
+++ yxb144325058汤浩浩/大作业/qiushibaike/NetDemo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  NetDemo
+//
+//  Created by ccr  on 14-12-6.
+//  Copyright (c) 2014年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class MainViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) MainViewController *viewController;
+
+@end
